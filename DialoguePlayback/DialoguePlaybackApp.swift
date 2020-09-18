c6ea238b3536d70ec49ba7b2ea5a0012a5d9b417
Oleg Shamin <shaminoleg@gmail.com>
2020-09-18 22:02:59 +0300

    Initial Commit

---
+++ DialoguePlayback/DialoguePlaybackApp.swift
@@ -0,0 +1,17 @@
+//
+//  DialoguePlaybackApp.swift
+//  DialoguePlayback
+//
+//  Created by Oleg Shamin on 18.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct DialoguePlaybackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
